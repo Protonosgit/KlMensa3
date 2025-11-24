2a0769dd862a88a510aa472e6347fa42b5074a97
Protonosgit <linuslinz98@gmail.com>
2025-11-24 11:57:23 +0100

    Testing new auth system

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,11 +1,19 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const params = new URLSearchParams({
-    redirect_uri: `${process.env.NEXT_PUBLIC_CURRENT_DOMAIN}/api/auth/callback`,
-    application_id: "klmensa_v3ext_user",
+  const CSRF_TOKEN = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  const loginUrl = `${process.env.LEGACY_API_URL}/api/v1/authorize-client?client_uuid=${process.env.LEGACY_API_URL_APP_UUID}&csrf_token=${CSRF_TOKEN}`;
+  
+  const res = NextResponse.redirect(loginUrl);
+    res.cookies.set({
+    name: "csrf_token",
+    value: CSRF_TOKEN,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    path: "/",
+    maxAge: 300
   });
 
-  const loginUrl = `${process.env.LEGACY_API_URL}/external_auth/authenticate.php?${params.toString()}`;
-  return NextResponse.redirect(loginUrl);
+  return res;
 }
