03fd3a39e4524f33bc71bbfb1df3163a79a5495b
Linus Linz <linuslinz98@gmail.com>
2025-12-22 16:20:58 +0100

    Fix veganuary tag

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -22,7 +22,7 @@ export default function ReportPage() {
       </header>
 
       <main className={shared.main}>
-        <h3 className={styles.subtitle}>About this page</h3>
+        <h3 className={styles.subtitle}>About this page:</h3>
         <p className={styles.description}>
           Mensa KL was created in 2008 by <a href="https://www.johannesschildgen.de/">Johannes Schildgen</a>. Since then it's been offering images and ratings about the current meals on the menu. 
           The current owner and maintainer of the project is <a href="https://www.instagram.com/mensaklde/">Dennis Meckel</a>.<br/> 
@@ -30,16 +30,16 @@ export default function ReportPage() {
           Feel free to check out the code on <a href="https://github.com/Protonosgit/KlMensa3">Github</a> and leave a star if you feel generous!<br/>
           Because of the nature of the project, we rely entirely on community support and positive user interactions. Thank you for your contributions!
         </p>
-        <h3 className={styles.subtitle}>Developed by</h3>
+        <h3 className={styles.subtitle}>V2 Developed By:</h3>
         <div className={styles.profiles}>
-          <a href="https://felixlenz.de/" className={styles.profileBox}>
+          <a href="https://lenz-felix.de/" className={styles.profileBox}>
             <CodeIcon className={styles.codeIcon} />
             <p className={styles.profileName}>Felix Lenz</p>
-            <p className={styles.roleChip}>V2 Developer</p>
+            <p className={styles.roleChip}>Admin</p>
           </a>
 
         </div>
-        <h3 className={styles.subtitle}>Socials</h3>
+
       </main>
     </div>
   )
