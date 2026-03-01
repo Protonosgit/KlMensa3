a09564e4b38dfff415b09d4331f35c79a2c34b87
Protonosgit <linuslinz98@gmail.com>
2026-03-01 16:07:40 +0100

    Small changes to ai

--- src/components/DetailsModal.js
+++ src/components/DetailsModal.js
@@ -14,6 +14,7 @@ import {
   Bot,
   Clock10Icon,
   FlagIcon,
+  Flame,
   InfoIcon,
   Leaf,
   MaximizeIcon,
@@ -352,6 +353,7 @@ const MealTitle = () => {
               ) : (
                 ""
               )}
+              {meal?.dpartname == "Grill" ? <Flame size={20} className={styles.otherIcon} /> : ""}
             </p>
           </div>
 
